79d24c36d91e96603681d860b30db7437da5fed3
getlis <getprog777@gmail.com>
2023-11-28 23:39:30 +0300

    SNAPSHOT

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,17 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean =  (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat cat1 = (Cat) applicationContext.getBean( "cat" );
+        System.out.println( cat1.getMessage() );
+
+        Cat cat2 = (Cat) applicationContext.getBean( "cat" );
+        System.out.println( cat2.getMessage() );
+
     }
 }
